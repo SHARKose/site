4518db9d0af9951ca35a2cefd03b154c9f816d71
SHARK <maks.blinov07@gmail.comm>
2026-06-13 01:11:17 +0300

    Fix: Optimize prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+// Это "синглтон" — он гарантирует, что при разработке (hot reload)
+// не создастся сотня подключений к базе, которые "съедят" лимит Neon.
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
